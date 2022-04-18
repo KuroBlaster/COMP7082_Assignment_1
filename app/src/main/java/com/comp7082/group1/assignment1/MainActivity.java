e12a98b16f2d5656828729a0c211b4fb75421f70
Voicu <voicu_n@outlook.com>
2022-04-17 23:49:14 -0700

    Initial app with basic photo gallery, navigation and captation editing functionality

---
+++ app/src/main/java/com/comp7082/group1/assignment1/MainActivity.java
@@ -0,0 +1,125 @@
+package com.comp7082.group1.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    private ArrayList<String> photos = null;
+    private int index = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void takePhoto(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.comp7082.group1.assignment1", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+    private ArrayList<String> findPhotos() {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(), "/Android/data/com.comp7082.group1.assignment1/files/Pictures");
+        ArrayList<String> photos = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            for (File f : fList) {
+                photos.add(f.getPath());
+            }
+        }
+        return photos;
+    }
+    public void scrollPhotos(View v) {
+        if(photos == null) return;
+        updatePhoto(photos.get(index), ((EditText) findViewById(R.id.etCaption)).getText().toString());
+        photos.clear();
+        photos = findPhotos();
+        switch (v.getId()) {
+            case R.id.btnPrev:
+                if (index > 0) {
+                    index--;
+                }
+                break;
+            case R.id.btnNext:
+                if (index < (photos.size()-1)) {
+                index++;
+            }
+            break;
+            default:
+                break;
+        }
+
+        displayPhoto(photos.get(index));
+    }
+    private void displayPhoto(String path) {
+        ImageView iv = (ImageView) findViewById(R.id.ivGallery);
+        TextView tv = (TextView) findViewById(R.id.tvTimestamp);
+        EditText et = (EditText) findViewById(R.id.etCaption);
+        if (path == null || path =="") {
+            iv.setImageResource(R.mipmap.ic_launcher);
+            et.setText("Description");
+            tv.setText("Date and time");
+        } else {
+            iv.setImageBitmap(BitmapFactory.decodeFile(path));
+            String[] attr = path.split("_");
+            tv.setText("Date: "+attr[1] + " Time: " + attr[2]);
+            et.setText(attr[3]);
+        }
+    }
+    private File createImageFile() throws IOException {
+// Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_Description" + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg",storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void updatePhoto(String path, String caption) {
+        String[] attr = path.split("_");
+        if (attr.length >= 4) {
+            File to = new File(attr[0] + "_" + attr[1] + "_" + attr[2] + "_" + caption + "_" + attr[4]);
+            File from = new File(path);
+            from.renameTo(to);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            ImageView mImageView = (ImageView) findViewById(R.id.ivGallery);
+            mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+            displayPhoto(mCurrentPhotoPath);
+            photos = findPhotos();
+        }
+    }
+}
