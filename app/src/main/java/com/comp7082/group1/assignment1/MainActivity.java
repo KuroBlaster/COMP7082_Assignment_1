0a2275596fcead17b12dc7273e4dfc95d7d3536f
Oscar <au.oscar@hotmail.com>
2022-04-20 02:22:21 -0700

    all requirements features  should be good

--- app/src/main/java/com/comp7082/group1/assignment1/MainActivity.java
+++ app/src/main/java/com/comp7082/group1/assignment1/MainActivity.java
@@ -2,6 +2,7 @@ package com.comp7082.group1.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -12,12 +13,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String mCurrentPhotoPath;
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        photos = findPhotos();
+        photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
         if (photos == null) {
             displayPhoto(null);
         } else {
@@ -54,24 +58,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private ArrayList<String> findPhotos() {
+    private ArrayList<String> findPhotos(Date startTimestamp, Date endTimestamp, String keywords) {
         File file = new File(Environment.getExternalStorageDirectory()
                 .getAbsolutePath(), "/Android/data/com.comp7082.group1.assignment1/files/Pictures");
         ArrayList<String> photos = new ArrayList<String>();
         File[] fList = file.listFiles();
-        if (fList != null) {
-            for (File f : fList) {
-                photos.add(f.getPath());
+        try {
+            if (fList != null) {
+                for (File f : fList) {
+                    if (((startTimestamp == null && endTimestamp == null) || (f.lastModified() >= startTimestamp.getTime()
+                            && f.lastModified() <= endTimestamp.getTime())
+                    ) && (keywords == "" || f.getPath().contains(keywords)))
+                        photos.add(f.getPath());
+                }
             }
+        }catch(Exception ex){
+//            Crashes after photo taken
         }
         return photos;
     }
 
     public void scrollPhotos(View v) {
-        if(photos == null) return;
-        updatePhoto(photos.get(index), ((EditText) findViewById(R.id.etCaption)).getText().toString());
-        photos.clear();
-        photos = findPhotos();
+//        if(photos == null) return;
+        if (photos.size() != 0) {
+            updatePhoto(photos.get(index), ((EditText) findViewById(R.id.etCaption)).getText().toString());
+        }
+//        photos.clear();
+//        photos = findPhotos();
         switch (v.getId()) {
             case R.id.btnPrev:
                 if (index > 0) {
@@ -79,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.btnNext:
-                if (index < (photos.size()-1)) {
+                if (index < (photos.size() - 1)) {
                     index++;
                 }
                 break;
@@ -101,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             iv.setImageBitmap(BitmapFactory.decodeFile(path));
             String[] attr = path.split("_");
-            tv.setText("Date: "+attr[1] + " Time: " + attr[2]);
+            tv.setText("Date: " + attr[1] + " Time: " + attr[2]);
             et.setText(attr[3]);
         }
     }
@@ -111,27 +124,60 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_Description" + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".jpg",storageDir);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
     private void updatePhoto(String path, String caption) {
         String[] attr = path.split("_");
+        Toast t2 = Toast.makeText(this, "" + index, Toast.LENGTH_LONG);
+        t2.show();
         if (attr.length >= 4) {
             File to = new File(attr[0] + "_" + attr[1] + "_" + attr[2] + "_" + caption + "_" + attr[4]);
             File from = new File(path);
             from.renameTo(to);
+            Toast t1 = Toast.makeText(this, "" + from.renameTo(to), Toast.LENGTH_LONG);
+//            t1.show();
+            photos.set(index, attr[0] + "_" + attr[1] + "_" + attr[2] + "_" + caption + "_" + attr[4]);
         }
+    }
+
+    public void launchSearchActivity(View view) {
+        Intent i = new Intent(this, SearchActivity.class);
+        startActivityForResult(i, 345);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            DateFormat format = new SimpleDateFormat("yyyy‐MM‐dd HH:mm:ss");
+            Date startTimestamp, endTimestamp;
+            try {
+                String from = (String) data.getStringExtra("STARTTIMESTAMP");
+                String to = (String) data.getStringExtra("ENDTIMESTAMP");
+                startTimestamp = format.parse(from);
+                endTimestamp = format.parse(to);
+            } catch (Exception ex) {
+                startTimestamp = null;
+                endTimestamp = null;
+            }
+            String keywords = (String) data.getStringExtra("KEYWORDS");
+            index = 0;
+            photos = findPhotos(startTimestamp, endTimestamp, keywords);
+            if (photos.size() == 0) {
+                displayPhoto(null);
+            } else {
+                displayPhoto(photos.get(index));
+            }
+        }
+
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            index = photos.size();
             ImageView mImageView = (ImageView) findViewById(R.id.ivGallery);
             mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
-            photos = findPhotos();
+            photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
         }
     }
 }
